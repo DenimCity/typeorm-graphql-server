6a13a7527814aa3e42a148241cfa30ee29c9c02d
E Altidor <ealtidor@Es-MacBook-Pro.attlocal.net>
2019-09-21 00:24:15 -0400

    changed to userId

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,11 +1,11 @@
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
-import { ObjectType, Field } from 'type-graphql';
+import { ObjectType, Field, ID } from 'type-graphql';
 
 @ObjectType()
 @Entity('users')
 export class User extends BaseEntity {
-	@Field()
-	@PrimaryGeneratedColumn() id: number;
+	@Field(() => ID)
+	@PrimaryGeneratedColumn() userId: number;
 	
 	@Field()
 	@Column() email: string;
