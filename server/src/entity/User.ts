0ad40ac9798b640bafc0e14ddaa3f03830fa442c
E Altidor <ealtidor@Es-MacBook-Pro.attlocal.net>
2019-09-20 22:07:03 -0400

    maded field queryable

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,9 +1,13 @@
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
+import { ObjectType, Field } from 'type-graphql';
 
+@ObjectType()
 @Entity('users')
 export class User extends BaseEntity {
+	@Field()
 	@PrimaryGeneratedColumn() id: number;
-
+	
+	@Field()
 	@Column() email: string;
 
 	@Column() password: string;
